55ff407a95cf8500a85e4d75de24550b55a15157
dart-wakar <wakar@whitelogic.in>
2017-08-05 23:35:28 +0530

    Genres info relations established

--- books/models.py
+++ books/models.py
@@ -8,14 +8,31 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=30)
 
+class BookSeries(models.Model):
+    title = models.CharField(max_length=50)
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     isbn = models.BigIntegerField(unique=True)
-    edition_language = models.CharField(max_length=15)
+    edition_language = models.CharField(max_length=15,null=True,blank=True)
     characters = models.ManyToManyField(Character)
+    book_series = models.ForeignKey(BookSeries,related_name='series',null=True,on_delete=models.SET_NULL)
+    part_in_series = models.IntegerField(null=True,blank=True)
+    author = models.ForeignKey(Author,related_name='author',null=True,on_delete=models.SET_NULL)
 
     class Meta:
         ordering = ('id',)
+
+class VotedGenre(models.Model):
+    genre = models.ForeignKey(Genre,related_name='genre')
+    votes = models.IntegerField(blank=True,default=0)
+    book = models.ForeignKey(Book,related_name='genres_info',null=True,on_delete=models.SET_NULL)
 
 class LiteraryAward(models.Model):
     name = models.CharField(max_length=100)
--- books/serializers.py
+++ books/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from books.models import Book,LiteraryAward
+from books.models import Book,LiteraryAward,VotedGenre
 
 class BookSerializer(serializers.ModelSerializer):
     awards = serializers.PrimaryKeyRelatedField(many=True,queryset=LiteraryAward.objects.all(),allow_null=True,required=False)
+    genres_info = serializers.PrimaryKeyRelatedField(many=True,queryset=VotedGenre.objects.all(),allow_null=True,required=False)
     class Meta:
         model = Book
-        fields = ('id','title','isbn','edition_language','characters','awards')
+        fields = ('id','title','isbn','edition_language','characters','awards','book_series','part_in_series','author','genres_info')
