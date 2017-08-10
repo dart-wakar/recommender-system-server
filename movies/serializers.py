746eb74e1dc712f2737612aa86c56a1bb6636144
dart-wakar <wakar@whitelogic.in>
2017-08-10 21:18:20 +0530

    Foreign key serialization

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from movies.models import Movie,MoviesCollection,ProductionCountry,ProductionCompany,SpokenLanguage,MovieGenre,Keyword
+from movies.models import Movie,MoviesCollection,ProductionCountry,ProductionCompany,SpokenLanguage,MovieGenre,Keyword,VotedMovieGenre
 
 class MoviesCollectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,9 +11,39 @@ class ProductionCountrySerializer(serializers.ModelSerializer):
         model = ProductionCountry
         fields = ('id','iso_3166_1','name')
 
+class ProductionCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductionCompany
+        fields = ('id','tmdb_id','name')
+
+class SpokenLanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpokenLanguage
+        fields = ('id','iso_639_1','name')
+
+class MovieGenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieGenre
+        fields = ('id','tmdb_id','name')
+
+class KeywordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = ('id','tmdb_id','name')
+
+class VotedMovieGenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VotedMovieGenre
+        fields = ('id','genre','votes','movie')
+
 class MovieSerializer(serializers.ModelSerializer):
     belongs_to_collection = MoviesCollectionSerializer()
     production_countries = ProductionCountrySerializer(many=True,read_only=True)
+    production_companies = ProductionCompanySerializer(many=True,read_only=True)
+    spoken_languages = SpokenLanguageSerializer(many=True,read_only=True)
+    genres_related = MovieGenreSerializer(many=True,read_only=True)
+    keywords = KeywordSerializer(many=True,read_only=True)
+    genres_info = VotedMovieGenreSerializer(many=True,allow_null=True,required=False)
     class Meta:
         model = Movie
-        fields = ('id','tmdb_id','original_title','title','poster_path','backdrop_path','adult','budget','homepage','original_language','overview','popularity','release_date','revenue','runtime','status','tagline','video','tmdb_average_rating','tmdb_vote_count','average_rating','votes_count','belongs_to_collection','production_countries','production_companies','spoken_languages','genres_related','keywords')
+        fields = ('id','tmdb_id','original_title','title','poster_path','backdrop_path','adult','budget','homepage','original_language','overview','popularity','release_date','revenue','runtime','status','tagline','video','tmdb_average_rating','tmdb_vote_count','average_rating','votes_count','belongs_to_collection','production_countries','production_companies','spoken_languages','genres_related','keywords','genres_info')
